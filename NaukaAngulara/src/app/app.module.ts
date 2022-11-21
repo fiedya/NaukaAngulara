4ba62232af9acd35a9b0f30ab00ea81ab4a8503f
SST\awinnicka <alicja.winicka@q-perior.pl>
2022-11-21 14:10:11 +0100

    theme almost works

--- NaukaAngulara/src/app/app.module.ts
+++ NaukaAngulara/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatCardModule} from '@angular/material/card'
+import {MatSelectModule} from '@angular/material/select'
+import {MatButtonModule} from '@angular/material/button'
+import {FormsModule} from "@angular/forms" 
 
-@NgModule({
+ @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatButtonModule, 
+    MatSelectModule,
+    MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
